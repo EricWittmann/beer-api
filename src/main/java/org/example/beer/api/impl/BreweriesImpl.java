411a5b4f7218f8e6d4d4c8f8ba3ca34617276ffd
Eric Wittmann <eric.wittmann@gmail.com>
2018-05-01 12:12:37 -0400

    added brewering GET impls

--- src/main/java/org/example/beer/api/impl/BreweriesImpl.java
+++ src/main/java/org/example/beer/api/impl/BreweriesImpl.java
@@ -1,12 +1,15 @@
 package org.example.beer.api.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import org.example.beer.api.Breweries;
 import org.example.beer.api.beans.Beer;
 import org.example.beer.api.beans.Brewery;
+import org.example.beer.api.data.BeerData;
 
 /**
  * Implementation of {@link Breweries}.
@@ -14,46 +17,65 @@ import org.example.beer.api.beans.Brewery;
 @ApplicationScoped
 public class BreweriesImpl implements Breweries {
 
+	@Inject
+	private BeerData beerData;
+
 	@Override
 	public List<Brewery> listAllBreweries(String state, String city) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			List<Brewery> breweries = beerData.listBreweries();
+			return breweries.stream().filter(beer -> {
+				if (state != null && !beer.getState().toLowerCase().contains(state.toLowerCase())) {
+					return false;
+				}
+				if (city != null && !beer.getCity().toLowerCase().contains(city.toLowerCase())) {
+					return false;
+				}
+				return true;
+			}).collect(Collectors.toList());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public void addBrewery(Brewery data) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public Brewery getBrewery(int breweryId) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return beerData.getBrewery(breweryId);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public void updateBrewery(int breweryId, Brewery data) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void deleteBrewery(int breweryId) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public List<Beer> listBreweryBeers(int breweryId) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			List<Beer> beers = beerData.listBeers();
+			return beers.stream().filter(beer -> {
+				if (beer.getBreweryId() == breweryId) {
+					return true;
+				}
+				return false;
+			}).collect(Collectors.toList());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public void addBeerToBrewery(int breweryId, Beer data) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
